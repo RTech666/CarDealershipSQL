f32d837967fa029376bd3e4c8f3579944c5ed60a
RTech666 <32269553+RTech666@users.noreply.github.com>
2024-06-11 13:55:50 -0400

    Added starting files from workshop 5.

---
+++ cardealershipsql/src/main/java/com/pluralsight/dealership/AddOn.java
@@ -0,0 +1,27 @@
+/* 
+AddOn.java
+This Java file contains the constructors and getters for the vehicle addons.
+*/
+
+package com.pluralsight.dealership;
+
+public class AddOn {
+    // Initalize the variables.
+    private String name;
+    private double price;
+
+    // Create the constructor.
+    public AddOn(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Create the getters.
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
---
+++ cardealershipsql/src/main/java/com/pluralsight/dealership/ContractFileManager.java
@@ -0,0 +1,47 @@
+/* 
+ContractFileManager.java
+This Java file allows the program to write the information into the contracts.csv file.
+*/
+
+package com.pluralsight.dealership;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ContractFileManager {
+    // Create the variable.
+    private static final String contractsFile = "contracts.csv";
+
+    // Create the saveContract method.
+    public void saveContract(Contract contract) {
+        try (FileWriter writer = new FileWriter(contractsFile, true)) {
+            // Use sale if sale, use lease if lease.
+            String contractType = (contract instanceof SalesContract) ? "SALE" : "LEASE";
+
+            // Write the information to the file.
+            writer.write(contractType + "|" +
+                    contract.getContractDate() + "|" +
+                    contract.getCustomerName() + "|" +
+                    contract.getCustomerEmail() + "|" +
+                    contract.getVehicleSold().getVin() + "|" +
+                    contract.getVehicleSold().getYear() + "|" +
+                    contract.getVehicleSold().getMake() + "|" +
+                    contract.getVehicleSold().getModel() + "|" +
+                    contract.getVehicleSold().getVehicleType() + "|" +
+                    contract.getVehicleSold().getColor() + "|" +
+                    contract.getVehicleSold().getOdometer() + "|" +
+                    contract.getVehicleSold().getPrice() + "|" +
+                    contract.getTotalPrice() + "|" +
+                    (contract instanceof SalesContract ? ((SalesContract) contract).getSalesTax() : "0.00") + "|" +
+                    (contract instanceof SalesContract ? ((SalesContract) contract).getRecordingFee() : "0.00") + "|" +
+                    (contract instanceof SalesContract ? ((SalesContract) contract).getProcessingFee() : "0.00") + "|" +
+                    (contract instanceof LeaseContract ? ((LeaseContract) contract).getLeaseFee() : "0.00") + "|" +
+                    (contract instanceof SalesContract ? (((SalesContract) contract).isFinanceOption() ? "YES" : "NO") : "NO") + "|" +
+                    (contract instanceof LeaseContract ? String.format("%.2f", ((LeaseContract) contract).getMonthlyPayment()) : "0.00") + "\n");
+
+            writer.flush();
+        // Print error if invalid input.
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
